e705069ea6143ee5181e01f89acebda7b9c209c5
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-07-19 14:47:56 +0200

    twenty fifth commit

---
+++ MyNEM/Chapter11/controllers/storeUserController.js
@@ -0,0 +1,10 @@
+const User = require('../models/User.js')
+const path = require('path')
+module.exports = (req, res) => {
+    User.create(req.body)
+        .then(user => res.redirect('/'))
+        .catch(error => {
+            console.log(error);
+            return res.redirect('/auth/register')
+        })
+}
--- MyNEM/Chapter11/index.js
+++ MyNEM/Chapter11/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const expressSession = require('express-session');
 const mongoose = require('mongoose')//page 53 - Connecting to MongoDB from Node
 
 mongoose.connect('mongodb://127.0.0.1/my_database', { useNewUrlParser: true })
@@ -11,6 +12,7 @@ const fileUpload = require('express-fileupload')
 const validateMiddleware = require('./middleware/validateMiddleware');
 //const customMiddleware = require('./middleware/customMiddleware')
 
+
 const newPostController = require('./controllers/newPostController')
 const homeController = require('./controllers/homeController')
 const storePostController = require('./controllers/storePostController')
@@ -21,21 +23,27 @@ const loginController = require('./controllers/loginController')
 const loginUserController = require('./controllers/loginUserController')
 
 app.use(express.static('public'))
+
+app.use(expressSession({
+    secret: 'keyboard cat'
+}))
+
 app.use(fileUpload())
 //app.use(customMiddleware)
 app.use(express.json()) //body parsing middleware
-app.use(express.urlencoded({ extended: true}));
-app.use('/posts/store',validateMiddleware) //to use validation middleware only to create posts
+app.use(express.urlencoded({ extended: true }));
+app.use('/posts/store', validateMiddleware) //to use validation middleware only to create posts
 
 
-app.get('/',homeController)
+
+app.get('/', homeController)
 app.get('/auth/register', newUserController)
 app.get('/auth/login', loginController);
 
 app.get('/posts/new', newPostController)
-app.get('/post/:id',getPostController)
+app.get('/post/:id', getPostController)
 app.post('/users/register', storeUserController)
-app.post('/users/login',loginUserController)
+app.post('/users/login', loginUserController)
 app.post('/posts/store', storePostController)
 
 
