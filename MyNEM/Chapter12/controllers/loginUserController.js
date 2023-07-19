35857b76d9607fa7e5618d397bf007fe57a71d0d
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-07-19 23:52:03 +0200

    twenty eigth commit

---
+++ MyNEM/Chapter12/controllers/loginUserController.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+module.exports = (req, res) => {
+    const { username, password } = req.body;
+    let validUser; //declare user to avoid error: user not defined
+
+    User.findOne({ username: username })
+        .then(user => {
+            if (user) {
+                validUser = user;
+                return bcrypt.compare(password, user.password);
+            } else {
+                console.log('Invalid username');
+            }
+        })
+        .then(same => {
+            if (same) {
+                req.session.userId = validUser._id; //assign ID to the user session - to know when user logged in
+                res.redirect('/');
+            } else {
+                console.log('Invalid password');
+                res.redirect('/auth/login');
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.redirect('/auth/login');
+        });
+};
---
+++ MyNEM/Chapter12/controllers/storeUserController.js
@@ -0,0 +1,17 @@
+const User = require('../models/User.js');
+const path = require('path');
+
+module.exports = (req, res) => {
+    User.create(req.body)
+        .then(user => {
+            res.redirect('/');
+        })
+        .catch(error => {
+            const validationErrors = Object.keys(error.errors).map(key =>
+                error.errors[key].message
+            );
+            console.log(error)
+            req.session.validationErrors = validationErrors; 
+            res.redirect('/auth/register');
+        });
+};
