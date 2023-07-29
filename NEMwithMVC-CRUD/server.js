954d5695000b4b0f8f894f554b51a3ee02115ca5
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-07-29 23:54:29 +0200

    43rd commit

--- NEMwithMVC-CRUD/server.js
+++ NEMwithMVC-CRUD/server.js
@@ -1,5 +1,6 @@
-const express = require('express');const app = express();
-
+const express = require('express');
+const bodyParser= require('body-parser')
+const app = express()
 
 //READ - R in CRUD
 app.get('/', (req, res) => {  
